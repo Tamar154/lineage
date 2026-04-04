c70e1bbf10c23939395c36e7eb299243ba83abfe
Tamar Revazishvili <tamar.d154@gmail.com>
2026-04-04 14:00:00 +0300

    fix(auth): handle non-existant user in verifyToken

--- backend/src/middleware/verifyToken.ts
+++ backend/src/middleware/verifyToken.ts
@@ -2,8 +2,9 @@ import type { Request, Response, NextFunction } from "express";
 import AppError from "../utils/AppError.js";
 import jwt from "jsonwebtoken";
 import type { JwtPayload } from "../types/jwt.js";
+import { prisma } from "../config/db.js";
 
-export const verifyToken = (
+export const verifyToken = async (
   req: Request,
   res: Response,
   next: NextFunction,
@@ -16,6 +17,20 @@ export const verifyToken = (
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
+    });
+
+    if (!user) {
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "strict",
+      });
+
+      throw new AppError("User no longer exists", 401);
+    }
+
     req.user = decoded;
     next();
   } catch {

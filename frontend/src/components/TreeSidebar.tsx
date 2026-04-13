bae0547d17af212e4aff776ff09907cb4ffc1db2
Tamar Revazishvili <tamar.d154@gmail.com>
2026-04-13 22:30:23 +0300

    - add search functionality to TreeSidebar - add functionality for `back to trees`

--- frontend/src/components/TreeSidebar.tsx
+++ frontend/src/components/TreeSidebar.tsx
@@ -1,8 +1,9 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import AppLogo from "./AppLogo";
 import { IoIosArrowBack } from "react-icons/io";
-
-import styles from "../styles/TreeSidebar.module.css";
 import type { Person } from "../services/personService";
+import styles from "../styles/TreeSidebar.module.css";
 
 type Props = {
   treeName: string;
@@ -10,10 +11,18 @@ type Props = {
 };
 
 const TreeSidebar = ({ treeName, persons }: Props) => {
+  const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredPersons = persons.filter((person) => {
+    const fullName = `${person.firstName} ${person.lastName}`.toLowerCase();
+    return fullName.includes(searchQuery.toLowerCase());
+  });
+
   return (
     <div className={styles.wrapper}>
       {/* <div className={styles.topSection}> */}
-      <button className={styles.backBtn}>
+      <button className={styles.backBtn} onClick={() => navigate("/trees")}>
         <IoIosArrowBack />
         <span>Back to Trees</span>
       </button>
@@ -29,11 +38,12 @@ const TreeSidebar = ({ treeName, persons }: Props) => {
           className={styles.searchInput}
           type="text"
           placeholder="Search person"
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
       </div>
 
       <div className={styles.personList}>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <div className={styles.personItem} key={person.id}>
             {person.firstName} {person.lastName}
           </div>

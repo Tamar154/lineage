59fb7e65c4796d8b1fa6f2ec097cec79d3d424d4
Tamar Revazishvili <tamar.d154@gmail.com>
2026-04-14 12:23:22 +0300

    feat: add CreatePersonModal component and integrate with TreePage for person creation

--- frontend/src/pages/TreePage.tsx
+++ frontend/src/pages/TreePage.tsx
@@ -1,17 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useEffectEvent } from "react";
 import { useParams } from "react-router-dom";
 import { getTreeById, type Tree } from "../services/treeService";
 import PersonDetailsPanel from "../components/PersonDetailsPanel";
 import TreeMainView from "../components/TreeMainView";
 import TreeSidebar from "../components/TreeSidebar";
 import styles from "../styles/TreePage.module.css";
-import { getAllPersons, type Person } from "../services/personService";
+import {
+  createPerson,
+  getAllPersons,
+  type Person,
+} from "../services/personService";
+import CreatePersonModal from "../components/CreatePersonModal";
+import type { PersonFormData } from "../types/PersonFormData";
 
 const TreePage = () => {
   const { treeId } = useParams();
   const [tree, setTree] = useState<Tree | null>(null);
   const [persons, setPersons] = useState<Person[]>([]);
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [showCreatePersonModal, setShowCreatePersonModal] = useState(false);
 
   useEffect(() => {
     if (!treeId) return;
@@ -42,14 +49,36 @@ const TreePage = () => {
     return <div>Loading...</div>;
   }
 
+  const handleCreatePerson = async (data: PersonFormData) => {
+    if (!treeId) return;
+
+    try {
+      const res = await createPerson({ treeId, data });
+      setPersons((prev) => [...prev, res.data]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.sidebar}>
         <TreeSidebar treeName={tree.name} persons={persons} />
       </div>
       <div className={styles.main}>
-        <TreeMainView persons={persons} onSelectPerson={setSelectedPerson} />
+        <TreeMainView
+          persons={persons}
+          onSelectPerson={setSelectedPerson}
+          onOpenCreatePerson={() => setShowCreatePersonModal(true)}
+        />
       </div>
+      {showCreatePersonModal && (
+        <CreatePersonModal
+          onClose={() => setShowCreatePersonModal(false)}
+          onCreate={handleCreatePerson}
+        />
+      )}
+
       {selectedPerson && (
         <div className={styles.details}>
           <PersonDetailsPanel
